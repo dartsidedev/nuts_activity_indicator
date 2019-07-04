e26bc20ca738c0178fcdee6d9de4ae4f6c056c72
Vince Varga <vargavince91@gmail.com>
2019-07-04 21:04:25 +0200

    👩🏻‍🏫 Add example folder
    To generate the example folder, I used the following command:
    
    flutter create --template app --org dev.dartside.nuts_and_bolts --project-name nuts_and_bolts_example --ios-language swift --android-language kotlin .

---
+++ example/android/app/src/main/kotlin/dev/dartside/nuts_and_bolts/nuts_and_bolts_example/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.dartside.nuts_and_bolts.nuts_and_bolts_example
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
