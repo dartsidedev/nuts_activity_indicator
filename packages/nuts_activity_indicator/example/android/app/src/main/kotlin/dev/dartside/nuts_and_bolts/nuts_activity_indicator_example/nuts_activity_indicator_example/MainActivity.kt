8fa75710906a36b0b585d5bb2473a8ae06f1766d
Vince Varga <vargavince91@gmail.com>
2019-11-04 20:33:52 +0100

    ⏳ Activity Indicator iss about to get its own package...
    This is a whole lot of flutter create boilerplate...

---
+++ packages/nuts_activity_indicator/example/android/app/src/main/kotlin/dev/dartside/nuts_and_bolts/nuts_activity_indicator_example/nuts_activity_indicator_example/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.dartside.nuts_and_bolts.nuts_activity_indicator_example.nuts_activity_indicator_example
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ packages/nuts_activity_indicator/test/nuts_activity_indicator_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:nuts_activity_indicator/nuts_activity_indicator.dart';
+
+void main() {
+  test('coming soon? I hope so!', () {
+    expect(1, 1);
+  });
+}
